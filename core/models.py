cc4928835d494d14b504add2745bfa09024a6a20
Marco A. Barragan Gomez <marcobarragan@Marcos-MacBook-Pro.local>
2020-06-09 18:35:17 -0700

    fourth commit: added cart

--- core/models.py
+++ core/models.py
@@ -31,11 +31,19 @@ class Item(models.Model):
             'slug': self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.item.title
+        return f"{self.quantity} of {self.item.title}"
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import item_list, product_page, checkout_page, HomeView, ItemDetailView
+from .views import item_list, product_page, checkout_page, HomeView, ItemDetailView, add_to_cart
 
 app_name = 'core'
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'), 
     path('product/<slug>/', ItemDetailView.as_view(), name='product-page'), 
     path('checkout/', checkout_page, name='checkout-page'), 
-    # path('#', home_page, name="home-page")
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
     
 ]
